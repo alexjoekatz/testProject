d565c6c6a79e4edd40b07e7211df8beffcd79656
Alex Joe <alex.joe@olemediagroup.com>
2015-09-18 13:08:44 +0200

    Initial Commit

---
+++ api/controllers/TestControllerController.js
@@ -0,0 +1,11 @@
+/**
+ * TestControllerController
+ *
+ * @description :: Server-side logic for managing testcontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/UseController.js
@@ -0,0 +1,11 @@
+/**
+ * UseController
+ *
+ * @description :: Server-side logic for managing uses
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
